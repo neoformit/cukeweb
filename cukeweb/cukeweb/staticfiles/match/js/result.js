0af6dfd9b3f9f2ad8aba10beb877cf726734b0ba
Cameron Hyde <pluckthechicken@hotmail.co.uk>
2020-06-21 06:28:52 +0000

    Removed old staticfile directory

--- cukeweb/cukeweb/staticfiles/match/js/result.js
+++
@@ -1,9 +0,0 @@
-function toggleResultSelect(button) {
-    if (button.innerText == "Select") {
-        $(button).html("&#215;");
-        $(button).parent().parent('tr').addClass('selected');
-    } else {
-        $(button).text("Select");
-        $(button).parent().parent('tr').removeClass('selected');
-    }
-}
