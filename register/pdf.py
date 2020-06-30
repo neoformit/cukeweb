02a0c6a9aaf7a2dd55bef0cfd6dca6eea52a8f0e
Cameron Hyde <pluckthechicken@hotmail.co.uk>
2020-06-30 18:15:13 +1000

    PDF rendering: replace wkhtmltopdf with a latex report template

--- register/pdf.py
+++ register/pdf.py
@@ -1,7 +1,7 @@
 """Take report data from session and render as PDF for download/browser."""
 
 import os
-import pdfkit
+import subprocess
 from django.conf import settings
 from django.template.loader import render_to_string
 
@@ -13,33 +13,30 @@ def render(request):
     """Render to html template and convert to PDF."""
     data = request.session['registration_data']
     filename = 'report_%s.pdf' % data['identifier']
-    report_path = os.path.join(
+    tex_fname = filename.replace('.pdf', '.tex')
+    tex_dir = os.path.join(
         settings.MEDIA_ROOT,
         'temp',
-        'reports',
-        filename
+        'latex'
     )
+    tex_path = os.path.join(tex_dir, tex_fname)
     report_uri = os.path.join(
         settings.MEDIA_URL,
         'temp',
-        'reports',
-        filename
+        'latex',
+        tex_fname.replace('.tex', '.pdf')
     )
-    sections = len(data['registered'] + data['failed'])
-    page_height = round(sections * 2.2)
-    logger.info("Printing to pdf with page height %scm" % page_height)
 
-    config = pdfkit.configuration(
-        wkhtmltopdf=bytes('/usr/bin/wkhtmltopdf', 'utf-8')
-    )
-    pdfkit.from_string(
-        render_to_string('register/report.html', data),
-        report_path,
-        options={
-            'disable-local-file-access': None,
-            'allow': '/srv/sites/cukeweb/cukeweb/media/',
-            'page-height': 100,
-        },
-        configuration=config,
-    )
+    # Render tex file from report template
+    tex = render_to_string('register/report.tex', data)
+    with open(tex_path, 'w') as f:
+        f.write(tex)
+
+    # Render tex file to pdf
+    subprocess.run([
+        'pdflatex',
+        '-output-directory', tex_dir,
+        tex_fname,
+    ])
+
     return report_uri
